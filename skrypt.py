c3436d618bfdcdbb6c79187dac1ea809d89b1491
Rawster <ml305203@student.polsl.pl>
2024-03-24 16:36:45 +0100

    yo

--- skrypt.py
+++ skrypt.py
@@ -1,24 +1,25 @@
+
 import nibabel as nib
 import numpy as np
 import matplotlib.pyplot as plt
+import os
+
+
+
 
 # Wczytaj plik NIfTI
 nifti_file = nib.load('a1.nii')
 
 # Pobierz dane z pliku NIfTI
-nifti_data = nifti_file.get_fdata()[:, :, :, 1]  # Wybierz drugi wymiar 3D
+nifti_data = nifti_file.get_fdata()
+multi_layer_data = np.zeros(nifti_data.shape)
+nifti_data_view = nifti_data[:,:,:,1]
 
-# Określenie progu intensywności (np. 90 percentyla)
-threshold = np.percentile(nifti_data, 90)
-
-# Stworzenie maski na podstawie progu intensywności
-mask = nifti_data >= threshold
-
+#voxels = np.random.randint(0, 2, size=(32, 32, 32))
 # Tworzenie figury i osi 3D
 fig = plt.figure()
 ax = fig.add_subplot(111, projection='3d')
 
-# Wyświetlenie tylko tych voxelów, które przekraczają próg
-ax.voxels(mask, edgecolor='k')
 
+ax.voxels(nifti_data_view, edgecolor='k')
 plt.show()
---
+++ test.py
@@ -0,0 +1,22 @@
+import voxelmap
+import nibabel as nib
+import numpy as np
+from model import Voxels
+
+def swap_values(array):
+    return np.where(array > 0, 1, array)
+
+nifti_file = nib.load('a1.nii')
+nifti_data = nifti_file.get_fdata()
+nifti_data_view = nifti_data[:,:,:,1]
+nifti_data_view = swap_values(nifti_data_view)
+model = voxelmap.Model(nifti_data_view)
+
+#VoxelsArray = np.array(Voxels)
+#model = voxelmap.Model(VoxelsArray)
+
+
+# map each voxel embedding (int) to a particular [color, alpha_transparency] combination
+model.hashblocks ={1: ['#bfff00',1], 2:['#c18bff',1], 3:['#37aa97',1],4:['magenta',1],5:['cyan',1]}
+
+model.draw(geometry="voxels")
